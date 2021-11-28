087a2373d7ef115b047ef0ecfccaa85ac7969a4c
adiawara <adiawara@stevens.edu>
2021-11-28 11:17:01 -0500

    Update test_interface.py

--- test_interface.py
+++ test_interface.py
@@ -1,48 +1,63 @@
 
 # -*- coding: utf-8 -*-
-"""
-Each Status-Code, including a description of which method(s)
-it can follow and any metainformation required in the response, is described in the below url link.
-w3.org/Protocols/rfc2616/rfc2616-sec10.html#:~:text=2%20201%20Created,by%20a%20Location%20header%20field.
-"""
+'''
+***** I read through the process of accomplishing this homework the following *****
+Using mock objects correctly goes against our intuition to make tests as real and
+thorough as possible, but doing so gives us the ability to write self-contained tests that
+run quickly, with no dependencies. It gives us the power to test exception handling and
+edge cases that would otherwise be impossible to test. Most importantly, it gives us
+the freedom to focus our test efforts on the functionality of our code, rather than
+our ability to set up a test environment. By concentrating on testing what’s important,
+we can improve test coverage and increase the reliability of our code, which is why
+we test in the first place.
 
+'''
 import unittest
+from unittest import mock 
+from unittest.mock import Mock, patch, MagicMock
 import interface
 from interface import*
 from interface import numberReposandCommits
 
 class TestGithubApi(unittest.TestCase):
     # Type of function selected by a Developer in a Tester mind
-
-    def test_requestStatus(self):
+    @mock.patch('interface.numberReposandCommits')
+    def test_mock_test_requestStatus(self, mock_status):
+        mock_status.return_value = MagicMock(status_code = 200)
+        #result = interface.numberReposandCommits(user)
+        result = mock_status.return_value.status_code
         try:
-            userDatatest = numberReposandCommits(user)
-            self.assertEqual(userDatatest["requestStatus"],200)
+            self.assertEqual(result, 200)
         except:
             print("Test Failed")
         else:
             print("Test succeed")
-    
-    def test_On_numberOfRepositories(self): 
+            
+    @mock.patch('interface.numberReposandCommits')
+    def test_mock_numberReposandCommits(self, mock_number):
+        mock_number.return_value = MagicMock(repository = 9)
+        #result = interface.numberReposandCommits(user)
+        result = mock_number.return_value.repository
         try:
-             userDatatest = numberReposandCommits(user)
-             self.assertEqual(userDatatest["numberOfRepos"],9)
+             self.assertEqual(result, 9)
         except:
             print("Test Failed")
         else:
             print("Test succeed")
-    
-    def test_userID(self):
-        try:
-             userDatatest = numberReposandCommits(user)
-             self.assertEqual(userDatatest["id"],"richkempinski")
-        except:
+    @mock.patch('interface.numberReposandCommits')
+    def test_mock_test_userID(self, mock_userID):
+         mock_userID.return_value = MagicMock(userID = "richkempinski")
+         #result = interface.numberReposandCommits(user)
+         result = mock_userID.return_value.userID
+         try:
+             self.assertEqual(result, "richkempinski")
+         except:
             print("Test Failed")
-        else:
+         else:
             print("Test succeed")
     
 if __name__ == '__main__':
     print('       unit tests  ....  ')
     unittest.main(exit=False)
 
-
+        
